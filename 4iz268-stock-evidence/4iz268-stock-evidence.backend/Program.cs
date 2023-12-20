fc096fe12ac1d731f5ce960b329315c1e71da3be
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-20 23:52:46 +0100

    added Microsoft.EntityFrameworkCore.Tools and Npgsql.EntityFrameworkCore.PostgreSQL to the project. Also prepared boilerplate for connection.

--- 4iz268-stock-evidence/4iz268-stock-evidence.backend/Program.cs
+++ 4iz268-stock-evidence/4iz268-stock-evidence.backend/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<someType>(options =>
+// {
+//     options.UseNpgsql(Environment.GetEnvironmentVariable(NameOfEnvVariable));
+// });
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
