7d168232b380748f06dbc7469e0230407a23d5d2
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-20 23:44:41 +0100

    corrected name in project

--- 4iz268-stock-evidence/4iz268-stock-evidence.backend/obj/Debug/net8.0/4iz268-stock-evidence.backend.AssemblyInfo.cs
+++ 4iz268-stock-evidence/4iz268-stock-evidence.backend/obj/Debug/net8.0/4iz268-stock-evidence.backend.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("4iz268-js-app.backnend")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("4iz268-stock-evidence.backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("4iz268-js-app.backnend")]
-[assembly: System.Reflection.AssemblyTitleAttribute("4iz268-js-app.backnend")]
+[assembly: System.Reflection.AssemblyProductAttribute("4iz268-stock-evidence.backend")]
+[assembly: System.Reflection.AssemblyTitleAttribute("4iz268-stock-evidence.backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
