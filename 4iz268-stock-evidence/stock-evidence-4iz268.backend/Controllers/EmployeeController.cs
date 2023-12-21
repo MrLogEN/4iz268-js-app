fe7ebedc243e79bc22a185c411df0bf5776d11fc
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 00:00:47 +0100

    Created CRUD methods for EmployeeController.cs

--- 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/EmployeeController.cs
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using stock_evidence.backend.Data;
+using stock_evidence.backend.Models;
 
 namespace stock_evidence.backend.Controllers;
 
@@ -13,5 +14,30 @@ public class EmployeeController: ControllerBase
     {
         _appDbContext = appDbContext;
     }
-    
+
+    [HttpPost]
+    public async Task<int> CreateEmployee([FromBody] Employee newEmployee)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet("{id}")]
+    public async Task<Employee?> GetEmployee(string id)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet]
+    public async Task<IEnumerable<Employee>> GetAllEmployees()
+    {
+        throw new NotImplementedException();
+    }
+    [HttpPut]
+    public async Task<int> UpdateEmployee([FromBody] Employee updatedEmployee)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpDelete("{id}")]
+    public async Task<int> DeleteEmployee(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
