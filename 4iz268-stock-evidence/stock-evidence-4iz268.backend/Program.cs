cf44023852a258bf3f43264d19d7173bc500548f
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 16:22:05 +0100

    Registered WarehouseService and MaterialService

--- 4iz268-stock-evidence/stock-evidence-4iz268.backend/Program.cs
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using stock_evidence.backend.Data;
+using stock_evidence.backend.Models;
+using stock_evidence.backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<EvidenceDbContext>(options =>
 {
     options.UseNpgsql(Environment.GetEnvironmentVariable("CON_STRING"));
 });
+builder.Services.AddSingleton<MaterialService>();
+builder.Services.AddSingleton<WarehouseService>();
 
 var app = builder.Build();
 
