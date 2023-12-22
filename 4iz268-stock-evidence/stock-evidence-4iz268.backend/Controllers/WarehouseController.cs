0a5c24356990c87a113bdd8c1e20a306d0933b17
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 16:55:22 +0100

    Defined methods for WarehouseController.cs

--- 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/WarehouseController.cs
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/WarehouseController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using stock_evidence.backend.Models;
+using stock_evidence.backend.Services;
 
 namespace stock_evidence.backend.Controllers;
 
@@ -6,5 +9,36 @@ namespace stock_evidence.backend.Controllers;
 [ApiController]
 public class WarehouseController : ControllerBase
 {
-    
+    private readonly WarehouseService _warehouseService;
+
+    public WarehouseController(WarehouseService warehouseService)
+    {
+        _warehouseService = warehouseService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateWarehouse([FromBody] Warehouse warehouse)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Warehouse>>> GetAllWarehouses()
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Warehouse?>> GetWarehouse(string id)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Warehouse?>> UpdateWarehouse(string id, [FromBody] Warehouse warehouse)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteWarehouse(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
