7ffe70602246c2bbe474e4f5609436364279f4a4
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 18:42:30 +0100

    Created MaterialController.cs and defined its methods.

---
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/MaterialController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using stock_evidence.backend.Models;
+using stock_evidence.backend.Services;
+
+namespace stock_evidence.backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MaterialController : ControllerBase
+{
+    private readonly MaterialService _materialService;
+
+    public MaterialController(MaterialService materialService)
+    {
+        _materialService = materialService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateMaterialAsync([FromBody] Material material)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Material>>> GetAllMaterialAsync()
+    {
+        throw new NotImplementedException();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Material?>> GetMaterialAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Material?>> UpdateMaterialAsync(string id, [FromBody] Material material)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> CreateMaterialAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
