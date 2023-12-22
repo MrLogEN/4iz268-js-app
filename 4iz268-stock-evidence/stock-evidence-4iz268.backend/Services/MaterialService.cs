a4fe7f8e541e140dcc168c8c1670930f30c47817
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 16:00:46 +0100

    Created MaterialService.cs

---
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Services/MaterialService.cs
@@ -0,0 +1,38 @@
+using System.Data;
+using System.Numerics;
+using Microsoft.EntityFrameworkCore;
+using stock_evidence.backend.Data;
+using stock_evidence.backend.Models;
+
+namespace stock_evidence.backend.Services;
+
+public class MaterialService
+{
+    private readonly EvidenceDbContext _conext;
+
+    public MaterialService(EvidenceDbContext conext)
+    {
+        _conext = conext;
+    }
+
+    public async Task<Material?> InsertMaterial(Material newMaterial)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<Material?> GetMaterial(string id)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<IEnumerable<Material>> GetAllMaterial()
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<Material?> UpdateMaterial(Material updated)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<Material?> DeleteMaterial(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
