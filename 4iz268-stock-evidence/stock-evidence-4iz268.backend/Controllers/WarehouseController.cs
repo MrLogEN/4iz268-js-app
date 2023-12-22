ac34d1a49637b2b15c3b62029bbe55683b6ad2b3
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-22 16:23:42 +0100

    Created WarehouseController.cs

---
+++ 4iz268-stock-evidence/stock-evidence-4iz268.backend/Controllers/WarehouseController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace stock_evidence.backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WarehouseController : ControllerBase
+{
+    
+}
