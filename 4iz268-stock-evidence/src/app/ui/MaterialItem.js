41ad7400c203a5b1177240701150b935f5765f1a
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-25 23:17:14 +0100

    Titles for material records and shortening strings

--- 4iz268-stock-evidence/src/app/ui/MaterialItem.js
+++ 4iz268-stock-evidence/src/app/ui/MaterialItem.js
@@ -3,13 +3,21 @@ import ToolsGroup from "@/app/ui/ToolsGroup";
 export default function MaterialItem({material}){
 
     const partialId = material.idMat.substring(0,10) + '...';
+    let partialDescription = material.descMat;
+    if (material.descMat.length > 16){
+        partialDescription = material.descMat.substring(0,15)+'...';
+    }
+    let partialName = material.nameMat;
+    if (material.nameMat.length > 16){
+        partialName = material.nameMat.substring(0,15)+'...';
+    }
 
     return(
         <li id={material.idMat} key={material.idMat} className='grid grid-cols-5 gap-5 py-2 border-b-[1.5px]'>
             <div title={material.idMat} className='col-start-1 col-end-1 px-2 text-sm'>{partialId}</div>
-            <div className='col-start-2 col-end-2 px-2 text-gray-500 text-sm'>{material.nameMat}</div>
-            <div className='col-start-3 col-end-3 text-center text-sm'>{material.descMat}</div>
-            <div className='col-start-4 col-end-4 text-center text-sm'>{material.unitMat}</div>
+            <div title={material.nameMat} className='col-start-2 col-end-2 px-2  text-sm'>{partialName}</div>
+            <div title={material.descMat} className='col-start-3 col-end-3 text-gray-500 text-sm'>{partialDescription}</div>
+            <div title={material.unitMat} className='col-start-4 col-end-4 text-center text-sm'>{material.unitMat}</div>
             <ToolsGroup itemId={material.idMat}></ToolsGroup>
         </li>
     )
