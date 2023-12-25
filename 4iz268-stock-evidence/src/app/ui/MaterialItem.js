d1faccec442e1e4bcf52b9f01fcdb8563ac17b90
Vilém Charwot <mrlogen1299@gmail.com>
2023-12-25 23:09:42 +0100

    Titles(cursor hover) and ID section created.

--- 4iz268-stock-evidence/src/app/ui/MaterialItem.js
+++ 4iz268-stock-evidence/src/app/ui/MaterialItem.js
@@ -1,11 +1,15 @@
 import ToolsGroup from "@/app/ui/ToolsGroup";
 
 export default function MaterialItem({material}){
+
+    const partialId = material.idMat.substring(0,10) + '...';
+
     return(
-        <li id={material.idMat} key={material.idMat} className='grid grid-cols-4 gap-5 py-2 border-b-[1.5px]'>
-            <div className='col-start-1 col-end-1 px-2 '>{material.nameMat}</div>
-            <div className='col-start-2 col-end-2 px-2 text-gray-500'>{material.descMat}</div>
-            <div className='col-start-3 col-end-3 text-center'>{material.unitMat}</div>
+        <li id={material.idMat} key={material.idMat} className='grid grid-cols-5 gap-5 py-2 border-b-[1.5px]'>
+            <div title={material.idMat} className='col-start-1 col-end-1 px-2 text-sm'>{partialId}</div>
+            <div className='col-start-2 col-end-2 px-2 text-gray-500 text-sm'>{material.nameMat}</div>
+            <div className='col-start-3 col-end-3 text-center text-sm'>{material.descMat}</div>
+            <div className='col-start-4 col-end-4 text-center text-sm'>{material.unitMat}</div>
             <ToolsGroup itemId={material.idMat}></ToolsGroup>
         </li>
     )
--- 4iz268-stock-evidence/src/app/ui/ToolsGroup.js
+++ 4iz268-stock-evidence/src/app/ui/ToolsGroup.js
@@ -3,7 +3,7 @@ import TrashButton from "@/app/ui/TrashButton";
 
 export default function ToolsGroup({itemId}){
     return(
-        <div className='flex flex-row col-start-4 col-end-4'>
+        <div title='Edit' className='flex flex-row col-start-5 col-end-5'>
             <EditButton itemId={itemId}></EditButton>
             <TrashButton itemId={itemId}></TrashButton>
         </div>
