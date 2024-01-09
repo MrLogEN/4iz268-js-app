6d4132051fba39d7c791b068b2b212aa6d989024
Vilém Charwot <mrlogen1299@gmail.com>
2024-01-09 13:43:01 +0100

    Creating stock implemented.

--- 4iz268-stock-evidence/src/app/lib/CreateNewStock.js
+++ 4iz268-stock-evidence/src/app/lib/CreateNewStock.js
@@ -2,4 +2,29 @@
 
 export async function CreateNewStock(status, formData){
 
+    const input = formData.get('idWrhs');
+    const select = formData.get('warehouses');
+
+    const data = {
+        WarehouseId: input?.length < 1 ? select : input,
+        MaterialId: formData.get('idMat'),
+        Quantity: formData.get('quantity')
+    }
+    const apiRoute = process.env.STOCK_API_ROUTE;
+    const url = `${apiRoute}/api/stock`;
+
+    const stringData = JSON.stringify(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        cache: "no-cache",
+        body:stringData,
+        headers:{
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    });
+    if (response.ok){
+        return {message: 'Stock created', style: 'text-green-700'}
+    }
+    return {message: `${response.status} Failed to create stock`, style: 'text-red-700'}
+    //return {message: `${stringData} Failed to create stock`, style: 'text-red-700'}
 }
--- 4iz268-stock-evidence/src/app/ui/StockForm.js
+++ 4iz268-stock-evidence/src/app/ui/StockForm.js
@@ -1,8 +1,6 @@
 'use client'
-
-import {SelectWarehouseOptions} from "@/app/ui/SelectWarehouseOptions";
 import {useFormState, useFormStatus} from "react-dom";
-import CreateNewMaterial from "@/app/lib/CreateNewMaterial";
+import {CreateNewStock} from "@/app/lib/CreateNewStock";
 
 const initialState = {
     message: null
@@ -17,14 +15,14 @@ function SubmitButton(){
 
 export default function StockForm({warehouseOptions}){
 
-    const [state, formAction] = useFormState(CreateNewMaterial, initialState)
+    const [state, formAction] = useFormState(CreateNewStock, initialState)
 
     return (
         <form className='flex flex-col gap-2' action={formAction}>
             <label htmlFor='idWrhs'>Warehouse: </label>
             <div className='flex flex-row self-stretch'>
                 <input id='idWrhs' alt='warehouse id' name='idWrhs' className='border-2 rounded-md grow' type='text'
-                       placeholder={'aa11-ff2-xyz'} maxLength={36} required></input>
+                       placeholder={'aa11-ff2-xyz'} maxLength={36}></input>
                 {warehouseOptions ? warehouseOptions : <p>Failed to load warehouses!</p>}
             </div>
             <label htmlFor='idMat'>Material ID: </label>
@@ -34,9 +32,7 @@ export default function StockForm({warehouseOptions}){
             <input id='quantity' alt='material quantity in the warehouse' name='quantity'
                    className='border-2 rounded-md' type='number'
                    placeholder={'250'} min={0} required></input>
-            {/*<input type='select'>Kokot<input/>*/}
             <div>
-                {/*<button type='submit' className='border-2 px-4 py-2 rounded-md bg-blue-700 text-white'>Create</button>*/}
                 <SubmitButton></SubmitButton>
             </div>
             <p aria-live='polite' role='status' className={state?.style}>
