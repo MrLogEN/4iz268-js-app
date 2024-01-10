8d4ab2d45cf5ae5595c211070de1f46d99e0dd01
Vilém Charwot <mrlogen1299@gmail.com>
2024-01-10 15:04:36 +0100

    Created layout.js, page.js, loading.js for Edit Stock. Not final

---
+++ 4iz268-stock-evidence/src/app/dashboard/edit/stock/layout.js
@@ -0,0 +1,9 @@
+export default function EditStockLayout({children}){
+    return(
+        <>
+            <h2 className='font-bold'>Edit stock</h2>
+            {children}
+        </>
+
+    )
+}
---
+++ 4iz268-stock-evidence/src/app/dashboard/edit/stock/loading.js
@@ -0,0 +1,5 @@
+export default function EditStockLoading(){
+    return(
+        <p>Loading stock...</p>
+    )
+}
