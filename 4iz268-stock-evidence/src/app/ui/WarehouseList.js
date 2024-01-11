f8794d762f21a2895c81930c145e13f2ad75a3bd
Vilém Charwot <mrlogen1299@gmail.com>
2024-01-11 17:41:22 +0100

    If the list is empty, return early with Nothing here yet message.

--- 4iz268-stock-evidence/src/app/ui/WarehouseList.js
+++ 4iz268-stock-evidence/src/app/ui/WarehouseList.js
@@ -3,6 +3,13 @@ import WarehouseItem from "@/app/ui/WarehouseItem";
 
 export default async function WarehouseList(){
     const rawList =  await GetWarehouses();
+    if (!rawList){
+        return (
+            <p>
+                Nothing here yet.
+            </p>
+        )
+    }
     const resultList = [];
     rawList.forEach(w => resultList.push(
        <WarehouseItem warehouse={w}></WarehouseItem>
